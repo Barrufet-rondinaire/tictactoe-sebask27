375158a66d0e5aeb0ae7313796d462245b7bbbf5
‘Vsevolod’ <vkopylov@cendrassos.net>
2025-03-26 16:50:42 +0100

    1r Dia

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace TicTacToe;
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
-class Program
+namespace TicTacToe
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static async Task Main()
+        {
+            try
+            {
+                var client = new HttpClient();
+                var response = await client.GetAsync("http://localhost:8080/jugadors");
+                var data = await response.Content.ReadAsStringAsync();
+
+                var matches = Regex.Matches(data, @"participant\s+""(.+?)""\s+""(.+?)""");
+                Console.WriteLine("Participantes:");
+                foreach (Match m in matches)
+                    Console.WriteLine($"- {m.Groups[1].Value} ({m.Groups[2].Value})");
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+        }
     }
 }
